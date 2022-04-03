1f2f6971d2dad7f3388ab9f24d4748ea49f39960
LucasEBSkora <skora.lucas@gmail.com>
2022-04-03 19:54:13 -0300

    termina ULA e testes

--- ULA.vhd
+++ ULA.vhd
@@ -3,18 +3,34 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all; 
 
 entity ULA is
-	port ( entr0,entr1 : in unsigned(15 downto 0);
-				sel_op : in unsigned(1 downto 0);
-				saida : out unsigned(15 downto 0)
+	port ( 
+				entr0, entr1 : in unsigned(15 downto 0);
+				sel_op : in unsigned(2 downto 0);
+				saida : out unsigned(15 downto 0);
+				op_zero, entr0_maior, entr0_menor : out std_logic
 			);
 end entity;
 
 architecture a_ULA of ULA is
+	signal saida_interno : unsigned(15 downto 0);
 begin
-	saída <= entr0+entr1 when sel_op = "00" else
-				entr0-entr1 when sel_op = "01" else
-				entr0 when entr0>entr1 and sel_op = "10" else
-				entr1 when entr1>entr0 and sel_op = "10" else  --retorna o maior
-				entr0(7 downto 0) and entr1(7 downto 0) when sel_op = '11' else --Operação de and em cada um dos bits
-				'0000000000000000';
-end
+	saida_interno <= 
+				entr0 + entr1															when sel_op = "000" else --a+b
+				entr0 - entr1 														when sel_op = "001" else --a-b
+				entr0(15 downto 0) or  entr1(15 downto 0) when sel_op = "010" else --a|b
+				entr0(15 downto 0) and entr1(15 downto 0) when sel_op = "011" else --a&b
+				entr0(15 downto 0) xor entr1(15 downto 0) when sel_op = "100" else --a^b
+				not entr0(15 downto 0) 										when sel_op = "101" else --!a
+				entr0																			when entr0 >  entr1 and sel_op = "110" else -- a > b ? a : b
+				entr1																			when entr0 <= entr1 and sel_op = "110" else -- a > b ? a : b
+				entr0																			when entr0 <  entr1 and sel_op = "111" else -- a < b ? a : b
+				entr1																			when entr0 >= entr1 and sel_op = "111" else -- a < b ? a : b
+				"0000000000000000";
+	saida <= saida_interno;
+	op_zero <= 	'1' when saida_interno = "0000000000000000" else
+							'0';
+	entr0_maior <=	'1' when entr0 > entr1 else
+									'0';
+	entr0_menor <=  '1' when entr0 < entr1 else
+									'0';
+end;
--- ULA_tb.vhd
+++ ULA_tb.vhd
@@ -6,47 +6,79 @@ entity ULA_tb is
 end;
 
 architecture a_ULA_tb of ULA_tb is
-	component mux8x1
-		port( entr0,entr1 : in unsigned(15 downto 0);
-				sel_op : in unsigned(1 downto 0);
-				saida : out unsigned(15 downto 0)
-			);
+	component ULA
+		port ( entr0, entr1 : in unsigned(15 downto 0);
+			sel_op : in unsigned(2 downto 0);
+			saida : out unsigned(15 downto 0);
+			op_zero, entr0_maior, entr0_menor : out std_logic
+		);
 	end component;
 	
-	signal entr0, entr1, in_sel0, in_sel1, in_sel2: std_logic;
+	signal entr0, entr1 : unsigned(15 downto 0);
+	signal sel_op : unsigned(2 downto 0);
+	signal saida : unsigned(15 downto 0);
+	signal op_zero, entr0_maior, entr0_menor : std_logic;
 
 	begin
-		uut: porta port map( entr0 => entr0,
-									entr1 => entr1,
-									sel_op => sel_op,
-									sel_1 => sel_1,
-									sel_2 => sel_2
-								);
-	process
-	begin
-		entr0 <= '0000000000000000';
-		entr1 <= '0000000000000000';
-		sel_op <= '00';
-		wait for 50 ns;
-		entr0 <= '0000000000000010';
-		entr1 <= '0000000000000100';
-		wait for 50 ns;
-		entr0 <= '0000000000000110';
-		entr1 <= '0000000000000100';
-		sel_op <= '01';
-		wait for 50 ns;
-		entr0 <= '0000000000000110';
-		entr1 <= '0000000000000100';
-		sel_op <= '10';
-		wait for 50 ns;
-		entr0 <= '0000000000000110';
-		entr1 <= '0000000000000100';
-		sel_op <= '01';
-		wait for 50 ns;
-		entr0 <= '1010101010101010';
-		entr1 <= '0101010101010101';
-		sel_op <= '11';
-		wait for 50 ns;
-		wait;
-	end process;
+		uut: ULA port map(entr0 => entr0, entr1 => entr1, sel_op => sel_op, saida => saida, op_zero => op_zero, entr0_maior => entr0_maior, entr0_menor => entr0_menor);
+		process
+		begin
+			sel_op <= "000";
+			entr0 <= "0000000000000000";
+			entr1 <= "0000000000000000";
+			wait for 50 ns;
+			entr0 <= "0000000000000011";
+			entr1 <= "0000000000000100";
+			wait for 50 ns;
+			entr0 <= "0000000000000011";
+			entr1 <= "1111111111111000";
+			wait for 50 ns;
+
+			sel_op <= "001";
+			entr0 <= "1111111111111111";
+			entr1 <= "1111111111111111";
+			wait for 50 ns;
+			entr0 <= "0000000011111111";
+			entr1 <= "0000000000000100";
+			wait for 50 ns;
+			entr0 <= "0000000011111111";
+			entr1 <= "0000000100000000";
+			wait for 50 ns;
+
+			sel_op <= "010";
+			entr0 <= "0101010101010101";
+			entr1 <= "1010101010101010";
+			wait for 50 ns;
+
+			sel_op <= "011";
+			wait for 50 ns;
+
+			sel_op <= "100";
+			entr0 <= "0000000011111111";
+			entr1 <= "0101010101010101";
+			wait for 50 ns;
+
+			sel_op <= "101";
+			wait for 50 ns;
+
+			sel_op <= "110";
+			entr0 <= "0000000000000011";
+			entr1 <= "0000000000000100";
+			wait for 50 ns;
+
+			entr0 <= "0000000000001011";
+			entr1 <= "0000000000000100";
+			wait for 50 ns;
+
+			sel_op <= "111";
+			entr0 <= "0000000000000011";
+			entr1 <= "0000000000000100";
+			wait for 50 ns;
+
+			entr0 <= "0000000000001011";
+			entr1 <= "0000000000000100";
+			wait for 50 ns;
+
+			wait;
+		end process;
 end architecture;
