b0a3767c49477b0168192fcf05946c9ea2152326
LucasEBSkora <skora.lucas@gmail.com>
2022-05-02 21:30:05 -0300

    muda ROM pra 15 bits, adiciona testbench

--- rom.vhd
+++ rom.vhd
@@ -3,28 +3,29 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity rom is
-    port( clk : in std_logic;
+    port( clk    : in std_logic;
         endereco : in unsigned(6 downto 0);
-        dado : out unsigned(11 downto 0)
+        dado     : out unsigned(14 downto 0)
     );
 end entity;
 
 architecture a_rom of rom is
-    type mem is array (0 to 127) of unsigned(11 downto 0);
+    type mem is array (0 to 127) of unsigned(14 downto 0);
     constant conteudo_rom : mem := (
-        0 => "000000000010",
-        1 => "100000000000",
-        2 => "000000000000",
-        3 => "000000000000",
-        4 => "100000000000",
-        5 => "000000000010",
-        6 => "111100000011",
-        7 => "000000000010",
-        8 => "000000000010",
-        9 => "000000000000",
-        10 => "000000000000",
+        0  => "000000000000010",
+        1  => "000100000000000",
+        2  => "000000000000000",
+        3  => "000000000000000",
+        4  => "000100000000000",
+        5  => "000000000000010",
+        6  => "000111100000011",
+        7  => "000000000000010",
+        8  => "000000000000010",
+        9  => "000000000000000",
+        10 => "000000000000000",
         others => (others=>'0')
     );
+    
 begin
     process(clk)
     begin
---
+++ tb_rom.vhd
@@ -0,0 +1,52 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity tb_rom is
+end entity tb_rom;
+
+architecture a_tb_rom of tb_rom is
+  component rom
+    port( clk    : in std_logic;
+        endereco : in unsigned(6 downto 0);
+        dado     : out unsigned(14 downto 0)
+    );
+  end component;
+
+  constant clk_period : time      := 100 ns;
+  signal finished     : std_logic := '0';
+  signal clk          : std_logic;
+  signal endereco     : unsigned(6 downto 0) := "0000000";
+  signal dado         : unsigned(14 downto 0);
+begin
+  uut: rom port map (clk => clk, endereco => endereco, dado => dado);
+
+    total_sim_time: process
+    begin
+      wait for 10 us;
+      finished <= '1';
+      wait;
+    end process total_sim_time;
+
+    clk_process: process
+    begin
+      while finished /= '1' loop
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+      end loop;
+      wait;
+    end process clk_process;
+  
+    testbench: process
+      begin
+        while finished /= '1' loop
+          wait for clk_period;
+        endereco <= endereco + "0000001";
+        end loop;
+        wait;
+    end process testbench;
+  
+  
+end architecture a_tb_rom;
